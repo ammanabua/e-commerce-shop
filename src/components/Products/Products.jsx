6b079b6f1e59b3b3a3b0ce54655d56ec42bd4212
Amman Abua <ammanabua@gmail.com>
2021-07-28 16:10:32 +0100

    add Products component with mock products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Basketball shoes', price: '$5'},
+    { id: 2, name: 'Bag', description: 'Fashion Bag', price: '$15'},
+]
+
+const Products = () => {
+    return(
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    );
+}
+
+export default Products;
