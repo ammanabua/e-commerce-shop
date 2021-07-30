693ceb10d32fd2f4c3f8e4558a1082554fe2a1f5
Amman Abua <ammanabua@gmail.com>
2021-07-30 14:06:57 +0100

    add cart layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -32,10 +33,21 @@ const App = () => {
     console.log(cart);
 
     return (
-        <div>
+        <Router>
+            <div>
             <Navbar totalItems={cart.total_items} />
-            <Products products={products} onAddToCart={handleAddToCart} />
+            <Switch>
+                <Route exact path="/">
+                    <Products products={products} onAddToCart={handleAddToCart} />
+                </Route>
+
+                <Route exact path="/cart">
+                    <Cart cart={cart} />
+                </Route>
+            </Switch>
+            
         </div>
+        </Router>
     )
 };
 
