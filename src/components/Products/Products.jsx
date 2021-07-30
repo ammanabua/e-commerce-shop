4caac9f80644c2161ebd2e6952ebbc6b6a419f47
Amman Abua <ammanabua@gmail.com>
2021-07-30 12:29:27 +0100

    add actual products using commercejs

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,18 +4,17 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Basketball shoes', price: '$5', image: 'bball.jpg'},
-    { id: 2, name: 'Bag', description: 'Fashion Bag', price: '$15', image: 'bag.jpg'},
-]
 
-const Products = () => {
+
+
+const Products = ({ products }) => {
     const classes = useStyles();
 
+    
     return(
         <main className={classes.content}>
             <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
