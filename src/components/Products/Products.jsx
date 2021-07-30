2bc178841857bf719c3e834ceb5262e09bf54836
Amman Abua <ammanabua@gmail.com>
2021-07-30 12:51:08 +0100

    add to cart functionality

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,17 +7,17 @@ import useStyles from './styles';
 
 
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
-    
+
     return(
         <main className={classes.content}>
             <div className={classes.toolbar} />
             <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
