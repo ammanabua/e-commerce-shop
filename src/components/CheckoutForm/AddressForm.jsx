4e09361e0cdc5370fceb35a9bc0b413a00037c2c
Amman Abua <ammanabua@gmail.com>
2021-08-01 13:52:16 +0100

    add buttons and functionality to address form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -7,7 +7,7 @@ import { commerce } from '../../lib/commerce';
 
 import FormInput from './FormInput';
 
-const AddressForm = ({ checkoutToken }) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -62,7 +62,7 @@ const AddressForm = ({ checkoutToken }) => {
         <>
             <Typography variant="h6" gutterBottom>Shipping Address</Typography>
             <FormProvider { ...methods}>
-                <form onSubmit=''>
+                <form onSubmit={methods.handleSubmit((data) => )}>
                     <Grid container spacing={3}>
                         <FormInput name='firstName' label='First name' />
                         <FormInput name='lastName' label='Last name' />
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,6 +11,7 @@ const Checkout = ({ cart }) => {
 
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
     const classes = useStyles();
 
     useEffect(() => {
@@ -28,6 +29,14 @@ const Checkout = ({ cart }) => {
         
     }, [cart]);
 
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1 );
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1 );
+
+    const next = (data) => {
+        setShippingData(data);
+        nextStep();
+    }
+
     const Confirmation = () => (
         <div>
             Confirmation
@@ -35,7 +44,7 @@ const Checkout = ({ cart }) => {
     );
 
     const Form = () => activeStep === 0 
-        ? <AddressForm checkoutToken={checkoutToken} />
+        ? <AddressForm checkoutToken={checkoutToken} next={next} />
         : <PaymentForm />
 
     
